7406920dd8b4f165dfd785b60cce580443d6d103
jcd3dr <102407900+jcd3dr@users.noreply.github.com>
2025-07-04 15:22:14 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,17 +2,25 @@
 
 <main id="main" class="site-main">
 
-    <?php
-    if ( have_posts() ) : 
-        while ( have_posts() ) : the_post();
-            the_title( '<h2>', '</h2>' );
-            the_content();
-        endwhile;
-    else :
-        echo '<p>No content found.</p>';
-    endif;
-    ?>
+<?php
+if ( have_posts() ) : 
+    while ( have_posts() ) : the_post();
 
+        // Obtenemos el valor de la casilla de verificación.
+        $hide_title = get_post_meta( get_the_ID(), '_hide_page_title_key', true );
+
+        // Solo mostramos el título si la casilla NO está marcada.
+        if ( '1' !== $hide_title ) {
+            the_title( '<h2>', '</h2>' );
+        }
+
+        the_content();
+
+    endwhile;
+else :
+    echo '<p>No content found.</p>';
+endif;
+?>
 </main>
 
 <?php get_footer(); ?>
